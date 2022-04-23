44c73189cee6cfdff3e1d9c8526d60ec25d627fc
hasan75 <hasanjab14@gmail.com>
2022-04-24 02:02:42 +0600

    login page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import "./App.css";
-import SideMenu, { menuItems } from "./components/Navigation/SideMenu";
+import './App.css';
+import SideMenu, { menuItems } from './components/Navigation/SideMenu';
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { useState } from "react";
-import ACPVerForm from "./components/ACPVerForm/ACPVerForm";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useState } from 'react';
+import ACPVerForm from './components/ACPVerForm/ACPVerForm';
+import Login from './pages/Login/Login';
 
-const Dashboard = () =><ACPVerForm/>;
+const Dashboard = () => <ACPVerForm />;
 const Content = () => <h1>Content</h1>;
 const Courses = () => <h1>Content/Courses</h1>;
 const Videos = () => <h1>Content/Videos</h1>;
@@ -17,38 +18,41 @@ const Design2 = () => <h1>Design 2</h1>;
 function App() {
   const [inactive, setInactive] = useState(false);
   return (
-    <div className="App">
+    <div className='App'>
       <Router>
-        <SideMenu
-          onCollapse={(inactive) => {
-            console.log(inactive);
-            setInactive(inactive);
-          }}
-        />
+        <Switch>
+          <Route exact path={'/'}>
+            <Login></Login>
+          </Route>
 
-        <div className={`container ${inactive ? "inactive" : ""}`}>
-          {/* improvememt, not recorded in video, its just looping through menuItems
-          instead of hard coding all the routes */}
-          {menuItems.map((menu, index) => (
-            <>
-              <Route key={menu.name} exact={menu.exact} path={menu.to}>
-                <h1>{menu.name}</h1>
-              </Route>
-              {menu.subMenus && menu.subMenus.length > 0
-                ? menu.subMenus.map((subMenu, i) => (
-                    <Route key={subMenu.name} path={subMenu.to}>
-                      <h1>{subMenu.name}</h1>
-                    </Route>
-                  ))
-                : null}
-            </>
-          ))}
+          <Route>
+            {/* <Switch> */}
+            <SideMenu
+              onCollapse={(inactive) => {
+                console.log(inactive);
+                setInactive(inactive);
+              }}
+            />
+            <div className={`containerSide ${inactive ? 'inactive' : ''}`}>
+              {menuItems.map((menu, index) => (
+                <>
+                  <Route key={menu.name} exact={menu.exact} path={menu.to}>
+                    <h1>{menu.name}</h1>
+                  </Route>
+                  {menu.subMenus && menu.subMenus.length > 0
+                    ? menu.subMenus.map((subMenu, i) => (
+                        <Route key={subMenu.name} path={subMenu.to}>
+                          <h1>{subMenu.name}</h1>
+                        </Route>
+                      ))
+                    : null}
+                </>
+              ))}
+            </div>
+            {/* </Switch> */}
+          </Route>
 
-          <Switch>
-            <Route exact path={"/"}>
-              <Dashboard />
-            </Route>
-            {/* <Route exact path={"/content"}>
+          {/* <Route exact path={"/content"}>
               <Content />
             </Route>
             <Route path={"/content/courses"}>
@@ -72,10 +76,8 @@ function App() {
             <Route path={"/design-2"}>
               <Design2 />
             </Route> */}
-          </Switch>
-        </div>
+        </Switch>
       </Router>
- 
     </div>
   );
 }
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,49 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import avatar from '../../assets/logo/swaconnec.png';
+import './Login.css';
 
 const Login = () => {
   const { register, handleSubmit } = useForm();
-  return <div></div>;
+
+  const onSubmit = (data) => {
+    // userLogin({ ...data, history, redirect });
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <div className='login-page'>
+        <div className=' d-flex align-items-center justify-content-center login-page-1'>
+          <div className='formContainer container'>
+            <div className='text-center mb-2'>
+              <img width='350px' src={avatar} alt='' />
+            </div>
+            <h2 className='text-center text-secondary mt-4'>User Access</h2>
+            <form className='loginForm' onSubmit={handleSubmit(onSubmit)}>
+              <input
+                className='form-control'
+                type='email'
+                required
+                {...register('email', { required: true })}
+                placeholder='Enter your email'
+              />
+              <input
+                required
+                type='password'
+                className='form-control'
+                {...register('password', { required: true })}
+                placeholder='Enter your password'
+              />
+              <button className='btn btn-primary btn-login' type='submit'>
+                Login
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;

e74f2796776b51032a5d79b921e0027e5c0935ac
hasan75 <hasanjab14@gmail.com>
2022-05-16 22:30:42 +0600

    vendor data fetched

--- src/pages/Vendors/Vendors.js
+++ src/pages/Vendors/Vendors.js
@@ -15,9 +15,35 @@ const Vendors = () => {
 
   const [vendors, setVendors] = useState([]);
 
+  useEffect(() => {
+    getVendors();
+  }, []);
+
+  const getVendors = () => {
+    fetch(`${url}`, {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-type': 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setVendors(data.data))
+      .catch((err) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Sorry',
+          text: 'error on device data fetching',
+        });
+      });
+  };
+
   return (
-    <div className='my-1'>
-      <h2>VEndor</h2>
+    <div className='mt-4 mb-2'>
+      <div className='d-flex justify-content-between align-items-center mb-2'>
+        <h2>Vendor</h2>
+        <button className='btn btn-outline-secondary'>Add A New Vendor</button>
+      </div>
+      <div className='my-1'>{/* table of Vendors */}</div>
     </div>
   );
 };
